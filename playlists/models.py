4291c675b2932bea6145f0d3443349d6138b2156
jihoon <jihoon6372@hanmail.net>
2018-03-27 21:08:46 +0900

    플레이리스트 모델 추가

--- playlists/models.py
+++ playlists/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from accounts.models import User
+from articles.models import Article
+from tracks.models import Track
 
 # Create your models here.
 class PlayList(models.Model):
-	"""docstring for ClassName"""
-	def __init__(self, arg):
-		super(ClassName, self).__init__()
-		self.arg = arg
-		
+	user = models.ForeignKey(User, verbose_name="등록자", null=True, on_delete=models.CASCADE, related_name='playlist')
+    track = models.ForeignKey(Track, verbose_name="곡정보", null=True, on_delete=models.CASCADE, related_name='playlist')
+	create_date = models.DateTimeField(auto_created=True, auto_now=True)
