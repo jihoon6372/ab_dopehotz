60483bc317edb84190367a246164a08b87f9c7b0
jihoon6372 <jihoon6372@hanmail.net>
2018-04-11 11:15:53 +0900

    플레이리스트 시리얼라이저 제공 필드 추가(id)

--- playlists/serializers.py
+++ playlists/serializers.py
@@ -19,68 +19,13 @@ class PersonSerializer(serializers.ModelSerializer):
 			'profile_picture',
 		)
 
-
-# class TrackSerializer(serializers.ModelSerializer):
-# 	user = PersonSerializer(read_only=True)
-	
-# 	def get_comment_count(self, obj):
-# 		return obj.comment.count()
-
-# 	# def get_comment(self, obj):
-# 	# 	c_qs = TrackComment.objects.filter_by_instance(obj)
-# 	# 	comments = TrackCommentSerializer(c_qs, many=True).data
-# 	# 	return comments
-
-# 	# comment = serializers.SerializerMethodField()
-# 	comment_count = serializers.SerializerMethodField()
-# 	# test_duration = 1000
-
-# 	class Meta:
-# 		model = Track
-# 		fields = (
-# 			'track_id',
-# 			'user',
-# 			'title',
-# 			'slug',
-# 			'tape_info',
-# 			'duration',
-# 			# 'test_duration',
-# 			'lyrics',
-# 			'hashtag',
-# 			'genre',
-# 			'image_url',
-# 			'download_url',
-# 			'waveform_url',
-# 			'view_count',
-# 			'likes',
-# 			'clips',
-# 			'track_score',
-# 			'on_stage',
-# 			'comment_count',
-# 			# 'comment',
-# 			'create_date'
-# 		)
-# 		read_only_fields = (
-# 			'user',
-# 			'slug',
-# 			'view_count',
-# 			'likes',
-# 			'clips',
-# 			'track_score',
-# 			'on_stage',
-# 			'create_date',
-# 			'genre',
-# 			'image_url',
-# 			'download_url',
-# 			'waveform_url'
-# 		)
-
 class PlayListSerializer(serializers.ModelSerializer):
     track = TrackSerializer()
 
     class Meta:
         model = PlayList
         fields = (
+			'id',
             'track',
             'order'
         )
