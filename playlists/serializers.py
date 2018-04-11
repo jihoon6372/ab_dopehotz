2b7e50c624a83c6dc296adfce0cc80ff0ea5171c
jihoon6372 <jihoon6372@hanmail.net>
2018-04-11 10:30:57 +0900

    플레이리스트 트랙 시리얼라이저 중복 제거 및 통합

--- playlists/serializers.py
+++ playlists/serializers.py
@@ -4,6 +4,8 @@ from .models import PlayList
 from rest_framework import serializers
 from social_django.models import UserSocialAuth
 from rest_framework.validators import UniqueTogetherValidator
+
+from track.serializers import TrackSerializer
 # from rest_framework.decorators import detail_route
 
 class PersonSerializer(serializers.ModelSerializer):
@@ -18,60 +20,60 @@ class PersonSerializer(serializers.ModelSerializer):
 		)
 
 
-class TrackSerializer(serializers.ModelSerializer):
-	user = PersonSerializer(read_only=True)
+# class TrackSerializer(serializers.ModelSerializer):
+# 	user = PersonSerializer(read_only=True)
 	
-	def get_comment_count(self, obj):
-		return obj.comment.count()
+# 	def get_comment_count(self, obj):
+# 		return obj.comment.count()
 
-	# def get_comment(self, obj):
-	# 	c_qs = TrackComment.objects.filter_by_instance(obj)
-	# 	comments = TrackCommentSerializer(c_qs, many=True).data
-	# 	return comments
+# 	# def get_comment(self, obj):
+# 	# 	c_qs = TrackComment.objects.filter_by_instance(obj)
+# 	# 	comments = TrackCommentSerializer(c_qs, many=True).data
+# 	# 	return comments
 
-	# comment = serializers.SerializerMethodField()
-	comment_count = serializers.SerializerMethodField()
-	# test_duration = 1000
+# 	# comment = serializers.SerializerMethodField()
+# 	comment_count = serializers.SerializerMethodField()
+# 	# test_duration = 1000
 
-	class Meta:
-		model = Track
-		fields = (
-			'track_id',
-			'user',
-			'title',
-			'slug',
-			'tape_info',
-			'duration',
-			# 'test_duration',
-			'lyrics',
-			'hashtag',
-			'genre',
-			'image_url',
-			'download_url',
-			'waveform_url',
-			'view_count',
-			'likes',
-			'clips',
-			'track_score',
-			'on_stage',
-			'comment_count',
-			# 'comment',
-			'create_date'
-		)
-		read_only_fields = (
-			'user',
-			'slug',
-			'view_count',
-			'likes',
-			'clips',
-			'track_score',
-			'on_stage',
-			'create_date',
-			'genre',
-			'image_url',
-			'download_url',
-			'waveform_url'
-		)
+# 	class Meta:
+# 		model = Track
+# 		fields = (
+# 			'track_id',
+# 			'user',
+# 			'title',
+# 			'slug',
+# 			'tape_info',
+# 			'duration',
+# 			# 'test_duration',
+# 			'lyrics',
+# 			'hashtag',
+# 			'genre',
+# 			'image_url',
+# 			'download_url',
+# 			'waveform_url',
+# 			'view_count',
+# 			'likes',
+# 			'clips',
+# 			'track_score',
+# 			'on_stage',
+# 			'comment_count',
+# 			# 'comment',
+# 			'create_date'
+# 		)
+# 		read_only_fields = (
+# 			'user',
+# 			'slug',
+# 			'view_count',
+# 			'likes',
+# 			'clips',
+# 			'track_score',
+# 			'on_stage',
+# 			'create_date',
+# 			'genre',
+# 			'image_url',
+# 			'download_url',
+# 			'waveform_url'
+# 		)
 
 class PlayListSerializer(serializers.ModelSerializer):
     track = TrackSerializer()
