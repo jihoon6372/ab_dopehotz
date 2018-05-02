f26690a829968cb7b2b134f382c513a03b00a048
jihoon6372 <jihoon6372@hanmail.net>
2018-05-02 15:35:18 +0900

    플레이리스트 수정시 로그기록 추가

--- playlists/views.py
+++ playlists/views.py
@@ -15,6 +15,7 @@ from django.db.models import Max
 
 from .models import PlayList
 from tracks.models import Track
+from logs.models import PlayListLog
 
 # Create your views here.
 def test_view(request):
@@ -43,3 +44,42 @@ class PlayListViewSet(viewsets.ModelViewSet):
 			order = order,
 			create_date=datetime.datetime.now()
 		)
+
+		# 로그 기록
+		log = PlayListLog()
+		log.track_id=track.id
+		log.user=self.request.user
+		log.order = order
+		log.log_id = 1
+		log.save()
+
+	
+	def update(self, request, *args, **kwargs):
+		instance = self.get_object()
+		serializer = self.get_serializer(instance, data=request.data)
+		serializer.is_valid(raise_exception=True)
+		self.perform_update(serializer)
+
+		# 로그 기록
+		log = PlayListLog()
+		log.track_id = instance.track_id
+		log.user = self.request.user
+		log.order = serializer.data['order']
+		log.log_id = 2
+		log.save()
+		return Response(serializer.data)
+
+
+	def destroy(self, request, *args, **kwargs):
+		instance = self.get_object()
+
+		# 로그 기록
+		log = PlayListLog()
+		log.track_id = instance.track_id
+		log.user = self.request.user
+		log.order = instance.order
+		log.log_id = 3
+		log.save()
+
+		self.perform_destroy(instance)
+		return Response(status=status.HTTP_204_NO_CONTENT)
