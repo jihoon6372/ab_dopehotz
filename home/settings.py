a23c7cc82db452f766b4f3a003f44405866fed8d
jihoon6372 <jihoon6372@hanmail.net>
2018-05-02 14:16:00 +0900

    setting.py 로그 앱 추가

--- home/settings.py
+++ home/settings.py
@@ -74,7 +74,7 @@ ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = LOGIN_REDIRECT_URL
 SECRET_KEY = 'zj7+c4182z+f$^t#76(x@l7&aytzjx7p-1aekeh%7ds0pd_sa7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 APPEND_SLASH = True
 # CSRF_COOKIE_SECURE = True
 
@@ -97,6 +97,7 @@ INSTALLED_APPS = [
     'tracks.apps.TracksConfig',
     'articles.apps.ArticlesConfig',
     'playlists.apps.PlaylistsConfig',
+    'logs',
 
     #third party
     'debug_toolbar',
